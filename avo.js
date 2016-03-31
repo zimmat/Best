fa1729c4e9ee1d6b81d646a496a0df5fe53c4ca3
zimmat <zimkhitha@projectcodex.co>
2016-03-31 12:08:22 +0200

    add

--- avo.js
+++ avo.js
@@ -1,7 +1,10 @@
-var string = "1 for R3, 2 for R7, 3 for R10, 5 for R14.50";
-var avocados = string.split(',');
+// module.exports = function(){
+var pricePerAvo = [];
+var avocado = "1 for R3, 2 for R7, 3 for R10, 5 for R14.50";
+var avocados = avocado.split(',');
 
+for (i = 0; i <= avocados.length;i++){
+  var avoo = avocado.split('for');
 
-
-
-console.log(avocados)
+}
+console.log(avoo);
