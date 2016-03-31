6584860b8c594d9428df42f8b47fb28f3d58449a
zimmat <zimkhitha@projectcodex.co>
2016-03-31 13:03:22 +0200

    add

--- avo.js
+++ avo.js
@@ -1,10 +1,24 @@
-// module.exports = function(){
+module.exports = function(){
 var pricePerAvo = [];
-var avocado = "1 for R3, 2 for R7, 3 for R10, 5 for R14.50";
+var avoo = "1 for R3, 2 for R7, 3 for R10, 5 for R14.50";
+var avocado = avoo
+    .replace("for", "")
+      .replace("for", "")
+        .replace("for", "")
+          .replace("for", "");
+for (i = 0; i <= avocado.length;i++){
 var avocados = avocado.split(',');
-
-for (i = 0; i <= avocados.length;i++){
-  var avoo = avocado.split('for');
-
 }
-console.log(avoo);
+
+console.log(avocados);
+
+var pricePerAvo = {};
+
+avocados.forEach(function(avocado){
+  var quantity = avocados.split(' ')[0];
+var price = avocados.split(' ')[1];
+if (pricePerAvo[avocados]) === undefined){
+  pricePerAvo[price] %= quantity;
+}
+}
+    }
